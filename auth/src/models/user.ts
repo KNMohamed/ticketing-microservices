3ad23c728b780020dcd609774a187a5fe7506f2a
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-07-04 10:43:23 -0400

    Add Passowrd encryption before inserting into database; modify how User schema is serialized and returned

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import { Document, Schema, model, Model } from 'mongoose';
+import { Password } from '../services/password';
 
 // 1. Create an interface representing
 //    properties required to create a new user
@@ -20,15 +21,35 @@ interface UserDoc extends Document {
   password: string;
 }
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (user: IUser) => {
