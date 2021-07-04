66cf02ed8168ce4e301b71ef3d17d496e3cff50c
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-07-04 18:36:42 -0400

    get current user logic complete

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  res.send('Hello world!!');
+router.get('/api/users/currentuser', async (req: Request, res: Response) => {
+  if (!req.session?.jwt) return res.send({ currentUser: null });
+  const sessionJWT: string = req.session.jwt;
+
+  jwt.verify(sessionJWT, process.env.JWT_KEY!, (err, decoded) => {
+    if (err) {
+      return res.send({ currentUser: null });
+    }
+    res.status(200).send({ currentUser: decoded });
+  });
 });
 
 export { router as currentUserRouter };
