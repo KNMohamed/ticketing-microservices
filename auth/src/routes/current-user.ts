ca2990714df34710185fcad1339689ca83dc59fd
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-07-05 23:55:38 -0400

    Create middlewares to get current user and ensure authentication and associated errors/routes

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,17 +1,14 @@
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
+
 const router = express.Router();
 
-router.get('/api/users/currentuser', async (req: Request, res: Response) => {
-  if (!req.session?.jwt) return res.send({ currentUser: null });
-  const sessionJWT: string = req.session.jwt;
-
-  jwt.verify(sessionJWT, process.env.JWT_KEY!, (err, decoded) => {
-    if (err) {
-      return res.send({ currentUser: null });
-    }
-    res.status(200).send({ currentUser: decoded });
-  });
-});
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  async (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
